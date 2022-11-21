ad289e2e0997f61f0abb6dbac3d448b187fbe102
Zuhaib Amjad <umar0423@gmail.com>
2022-11-21 18:49:10 +0500

    Add components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import { Routes, Route } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/countrydetails/:id" element={<CountryDetails />} />
+        <h1 className="text-3xl text-red-400 font-bold underline">
+          Hello world!
+        </h1>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <div>Homepage</div>
+    </>
+  );
+};
+
+export default Homepage;
