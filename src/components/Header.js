6fb74ce87d4981212636fe497aa02f236342d71d
Zuhaib Amjad <umar0423@gmail.com>
2022-11-22 06:51:11 +0500

    Add header on homepage and add styling to it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  //   return (
+  //     <div className="bg-glowing py-4">
+  //       <div className="sepia">
+  //         <img src={logo} alt="" width="80px" />
+  //       </div>
+  //       <h1>Hey</h1>
+  //     </div>
+  //   );
 };
 
 export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="bg-light text-white py-4 flex items-center justify-between">
       <p className="text-2xl ml-2 md:ml-8">2022</p>
-      <p className="text-white text-2xl">Countries Population</p>
+      <p className="text-white font-bold text-2xl">Countries Catalogue</p>
       <div className="flex text-lg gap-2 mr-2 md:mr-8">
         <FaMicrophone />
         <BsFillGearFill />
