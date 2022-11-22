048ad62f3768bee2b2aaf0c1a8d62d1caccfed3c
Zuhaib Amjad <umar0423@gmail.com>
2022-11-22 18:44:52 +0500

    Refactor homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import logo from '../images/logoCap.png';
+import { fetchCountries } from '../redux/countries';
+import CountryCard from './CountryCard';
 
 const Homepage = () => {
+  const dispatch = useDispatch();
+  const allCountries = useSelector((state) => state.countries);
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+  console.log(allCountries);
+  const countryElements = allCountries.map((country) => (
+    <CountryCard
+      key={country.countryId}
+      name={country.countryName}
+      id={country.countryId}
+      region={country.region}
+      flagImage={country.flag}
+      population={country.population}
+      area={country.area}
+    />
+  ));
   return (
-    <>
+    <div>
       <div className="bg-glowing flex items-center justify-evenly py-4">
         <div>
           <h1 className="text-lg text-white">Search Country by Name</h1>
@@ -13,8 +33,11 @@ const Homepage = () => {
           <img src={logo} alt="" width="80px" />
         </div>
       </div>
-      <div>Homepage</div>
-    </>
+
+      <ul className="country-ul grid grid-cols-1 sm:grid-cols-3  lg:grid-cols-5 w-full">
+        {countryElements}
+      </ul>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
