45c7bb25c43730dd0f76422faa601843d4fd0171
Zuhaib Amjad <umar0423@gmail.com>
2022-11-22 07:32:55 +0500

    Configure redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
